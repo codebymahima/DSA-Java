4c14f232c7059d76914b3a906f6560221ba1fe49
Mahima Lalwani <lalwni.mahi@gmail.com>
2026-07-14 22:34:57 +0530

    git commit -m "Built approach of removing duplicates in a sorted linkedlist"

---
+++ src/LinkedList/ListNode.java
@@ -0,0 +1,36 @@
+package LinkedList;
+// Remove duplicates from the sorted LinkedList and return the sorted list
+// problem 83 leetcode
+// in my first approach, I tried using slow fast pointers, and updated the list and performed deletions
+// the more optimised approach is to use only one node, and instead of changing the data of the nodes, we directly change the links
+// in array we update elements, in linkedlist we update the links of the nodes
+// either way time complexity = O(n)
+// space complexity = O(1)
+
+public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next == null){
+            return head;
+        }
+        ListNode current = head;
+
+        while(current!=null && current.next!=null){
+            if(current.val==current.next.val){
+                current.next = current.next.next;
+            }
+            else{
+                current = current.next;
+            }
+        }
+
+
+        return head;
+    }
+}
